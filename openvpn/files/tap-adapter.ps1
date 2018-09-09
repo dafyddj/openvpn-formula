2131c85b041d83c556a8d079de8141c7335eb958
Dafydd Jones <dafydd@techneg.it>
2018-09-09 22:32:39 +0100

    adapters: allow use of test=True for state application
    # Conflicts:
    #	openvpn/files/tap-adapter.ps1

--- openvpn/files/tap-adapter.ps1
+++ openvpn/files/tap-adapter.ps1
@@ -8,12 +8,6 @@ $tapDesc = 'TAP-Windows Adapter V9*'
 $tapDir = $env:ProgramFiles + "\TAP-Windows"
 $tapInstallCmd = "$tapDir\bin\tapinstall.exe"
 $tapInstallArgs = "install", "`"$tapDir\driver\OemVista.inf`"", "tap0901"     # Backtick quotes https://stackoverflow.com/q/17550663
-$testVerb = "would be "
-
-if (-not (Test-Path $tapInstallCmd)) {
-    Write-Host "changed=no comment=`'$tapInstallCmd is not available`'"
-    exit 1
-}
 
 if ($PSCmdlet.ParameterSetName -eq 'new') {
     if (Get-NetAdapter -InterfaceDescription $tapDesc | Where-Object Name -eq $New) {
@@ -21,22 +15,20 @@ if ($PSCmdlet.ParameterSetName -eq 'new') {
         exit
     }
     if ($PSCmdlet.ShouldProcess("$New")) {
-        $testVerb = ''
         if (-Not (Get-NetAdapter -InterfaceDescription $tapDesc | Where-Object Name -Like Eth*)) {
             $p = Start-Process $tapInstallCmd -ArgumentList $tapInstallArgs -NoNewWindow -Wait -PassThru
         }
         Get-NetAdapter -InterfaceDescription $tapDesc | Where-Object Name -Like Eth* `
             | Select-Object -First 1 | Rename-NetAdapter -NewName $New
     }
-    Write-Host "changed=yes comment=`'TAP-Windows adapter $New $($testVerb)created`'"
+    Write-Host "changed=yes comment=`'TAP-Windows adapter $New created`'"
 }
 
 if ($PSCmdlet.ParameterSetName -eq 'remove') {
     if ($PSCmdlet.ShouldProcess("$Remove")) {
-        $testVerb = ''
         # No practical way to remove individual TAP adapter, so rename randomly to get it "out of the way"
         Get-NetAdapter -Name $Remove | Rename-NetAdapter -NewName "tmp$(Get-Random -Min 10 -Max 999)"
     }
-    Write-Host "changed=yes comment=`'TAP-Windows adapter $Remove $($testVerb)removed`'"
+    Write-Host "changed=yes comment=`'TAP-Windows adapter $Remove removed`'"
 }
 
