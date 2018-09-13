1a784abfa55ef26299808bc73fd8ced88d22ac9b
Dafydd Jones <dafydd@techneg.it>
2018-09-13 19:03:53 +0100

    adapters: add different comments depending if running in test=True mode

--- openvpn/files/tap-adapter.ps1
+++ openvpn/files/tap-adapter.ps1
@@ -8,6 +8,7 @@ $tapDesc = 'TAP-Windows Adapter V9*'
 $tapDir = $env:ProgramFiles + "\TAP-Windows"
 $tapInstallCmd = "$tapDir\bin\tapinstall.exe"
 $tapInstallArgs = "install", "`"$tapDir\driver\OemVista.inf`"", "tap0901"     # Backtick quotes https://stackoverflow.com/q/17550663
+$testVerb = "would be "
 
 if ($PSCmdlet.ParameterSetName -eq 'new') {
     if (Get-NetAdapter -InterfaceDescription $tapDesc | Where-Object Name -eq $New) {
@@ -15,20 +16,22 @@ if ($PSCmdlet.ParameterSetName -eq 'new') {
         exit
     }
     if ($PSCmdlet.ShouldProcess("$New")) {
+        $testVerb = ''
         if (-Not (Get-NetAdapter -InterfaceDescription $tapDesc | Where-Object Name -Like Eth*)) {
             $p = Start-Process $tapInstallCmd -ArgumentList $tapInstallArgs -NoNewWindow -Wait -PassThru
         }
         Get-NetAdapter -InterfaceDescription $tapDesc | Where-Object Name -Like Eth* `
             | Select-Object -First 1 | Rename-NetAdapter -NewName $New
     }
-    Write-Host "changed=yes comment=`'TAP-Windows adapter $New created`'"
+    Write-Host "changed=yes comment=`'TAP-Windows adapter $New $($testVerb)created`'"
 }
 
 if ($PSCmdlet.ParameterSetName -eq 'remove') {
     if ($PSCmdlet.ShouldProcess("$Remove")) {
+        $testVerb = ''
         # No practical way to remove individual TAP adapter, so rename randomly to get it "out of the way"
         Get-NetAdapter -Name $Remove | Rename-NetAdapter -NewName "tmp$(Get-Random -Min 10 -Max 999)"
     }
-    Write-Host "changed=yes comment=`'TAP-Windows adapter $Remove removed`'"
+    Write-Host "changed=yes comment=`'TAP-Windows adapter $Remove $($testVerb)removed`'"
 }
 
